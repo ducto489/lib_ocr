5c1f70e6db810affefb728aac1914bd8526d564a
santapo <trtuann34@gmail.com>
2024-12-01 10:32:56 +0700

    add train and val script

---
+++ train.sh
@@ -0,0 +1,9 @@
+
+
+
+
+python cli.py train \
+    --backbone_name resnet18 \
+    --seq_name bilstm \
+    --pred_name ctc \
+    ...
---
+++ val.sh
@@ -0,0 +1,6 @@
+
+python cli.py validate \
+    --backbone_name resnet18 \
+    --seq_name bilstm \
+    --pred_name ctc \
+    ...
