c5e49d266281fe4854b09fcb64cd4744dcc09648
santapo <trtuann34@gmail.com>
2024-12-01 10:54:28 +0700

    update

--- data/dataset.py
+++ data/dataset.py
@@ -28,3 +28,7 @@ class OCRDataset(Dataset):
         if self.transform:
             image = self.transform(image)
         return image, label
+
+
+# TODO: function to get vocab from data
+VOCAB = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "â", "á"]
