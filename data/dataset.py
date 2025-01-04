78a90a5ffead95ecf0f36791d7013fb5726ef15d
PC <pmphuong1704@gmail.com>
2025-01-05 06:22:45 +0700

    model T1 dev

--- data/dataset.py
+++ data/dataset.py
@@ -1,9 +1,11 @@
-
-
 import cv2
 import json
 import os
+from PIL import Image
+import math
+import torch
 from torch.utils.data import Dataset
+import torchvision.transforms as transforms
 
 
 class OCRDataset(Dataset):
@@ -22,8 +24,9 @@ class OCRDataset(Dataset):
 
     def __getitem__(self, idx):
         image_name, label = self.data[idx]
-        image_path = os.path.join(self.data_path, 'images', image_name)
-        image = cv2.imread(image_path)
+        image_path = os.path.join(self.data_path, 'images', image_name + ".jpg")
+        # image = cv2.imread(image_path)
+        image = Image.open(image_path).convert('L')
         # TODO: add online augmentation pipeline
         if self.transform:
             image = self.transform(image)
@@ -31,4 +34,87 @@ class OCRDataset(Dataset):
 
 
 # TODO: function to get vocab from data
-VOCAB = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "â", "á"]
+def get_vocab(data_path):
+    tgt_path = os.path.join(data_path, 'labels.json')
+    with open(tgt_path, 'r') as f:
+        data = json.load(f)
+    data = data['labels']
+    vocab = set()
+    for label in data.values():
+        vocab.update(list(label))
+    return list(vocab)
+# VOCAB = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "â", "á"]
+
+class ResizeNormalize(object):
+
+    def __init__(self, size, interpolation=Image.BICUBIC):
+        self.size = size
+        self.interpolation = interpolation
+        self.toTensor = transforms.ToTensor()
+
+    def __call__(self, img):
+        img = img.resize(self.size, self.interpolation)
+        img = self.toTensor(img)
+        img.sub_(0.5).div_(0.5)
+        return img
+
+
+class NormalizePAD(object):
+
+    def __init__(self, max_size, PAD_type='right'):
+        self.toTensor = transforms.ToTensor()
+        self.max_size = max_size
+        self.max_width_half = math.floor(max_size[2] / 2)
+        self.PAD_type = PAD_type
+
+    def __call__(self, img):
+        img = self.toTensor(img)
+        img.sub_(0.5).div_(0.5)
+        c, h, w = img.size()
+        Pad_img = torch.FloatTensor(*self.max_size).fill_(0)
+        Pad_img[:, :, :w] = img  # right pad
+        if self.max_size[2] != w:  # add border Pad
+            Pad_img[:, :, w:] = img[:, :, w - 1].unsqueeze(2).expand(c, h, self.max_size[2] - w)
+
+        return Pad_img
+
+
+class AlignCollate(object):
+
+    def __init__(self, imgH=32, imgW=100, keep_ratio_with_pad=False):
+        self.imgH = imgH
+        self.imgW = imgW
+        self.keep_ratio_with_pad = keep_ratio_with_pad
+
+    def __call__(self, batch):
+        batch = filter(lambda x: x is not None, batch)
+        # import pdb; pdb.set_trace()
+        images, labels = zip(*batch)
+
+        if self.keep_ratio_with_pad:  # same concept with 'Rosetta' paper
+            resized_max_w = self.imgW
+            input_channel = 3 if images[0].mode == 'RGB' else 1
+            transform = NormalizePAD((input_channel, self.imgH, resized_max_w))
+
+            resized_images = []
+            for image in images:
+                w, h = image.size
+                ratio = w / float(h)
+                if math.ceil(self.imgH * ratio) > self.imgW:
+                    resized_w = self.imgW
+                else:
+                    resized_w = math.ceil(self.imgH * ratio)
+
+                resized_image = image.resize((resized_w, self.imgH), Image.BICUBIC)
+                resized_images.append(transform(resized_image))
+                # resized_image.save('./image_test/%d_test.jpg' % w)
+
+            image_tensors = torch.cat([t.unsqueeze(0) for t in resized_images], 0)
+
+        else:
+            
+            transform = ResizeNormalize((self.imgW, self.imgH))
+            image_tensors = [transform(image) for image in images] 
+            image_tensors = torch.cat([t.unsqueeze(0) for t in image_tensors], 0)
+
+        return image_tensors, labels
--- models/__init__.py
+++ models/__init__.py
@@ -2,36 +2,36 @@
 
 
 
-_backbone_factory = {
-    'resnet18': resnet18,
-    'resnet50': resnet50
-}
+# _backbone_factory = {
+#     'resnet18': resnet18,
+#     'resnet50': resnet50
+# }
 
 
-seq_factory = {
-    'bilstm': BiLSTM
-}
+# seq_factory = {
+#     'bilstm': BiLSTM
+# }
 
 
-pred_factory = {
-    'ctc': CTC,
-    'attn': Attention
-}
+# pred_factory = {
+#     'ctc': CTC,
+#     'attn': Attention
+# }
 
 
 
-def get_module(backbone_name, seq_name, pred_name):
-    if backbone_name not in _backbone_factory:
-        raise ValueError(f"Backbone {backbone_name} not found")
-    backbone = _backbone_factory[backbone_name]
+# def get_module(backbone_name, seq_name, pred_name):
+#     if backbone_name not in _backbone_factory:
+#         raise ValueError(f"Backbone {backbone_name} not found")
+#     backbone = _backbone_factory[backbone_name]
 
-    if seq_name not in seq_factory:
-        seq_module = None
-    else:
-        seq_module = seq_factory[seq_name]
+#     if seq_name not in seq_factory:
+#         seq_module = None
+#     else:
+#         seq_module = seq_factory[seq_name]
 
-    if pred_name not in pred_factory:
-        raise ValueError(f"Prediction {pred_name} not found")
-    pred_module = pred_factory[pred_name]
+#     if pred_name not in pred_factory:
+#         raise ValueError(f"Prediction {pred_name} not found")
+#     pred_module = pred_factory[pred_name]
 
-    return backbone, seq_module, pred_module
+#     return backbone, seq_module, pred_module
