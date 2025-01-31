a5e2a7d6685b57fdad2658076373b6cd84b74cda
mp1704 <pmphuong1704@gmail>
2025-02-01 04:58:16 +0700

    collate fn

--- data/collate.py
+++ data/collate.py
@@ -1,12 +1,15 @@
 import torch
 from torchvision import transforms
 
-class SynthCollator:
+
+class OCRCollator:
+
     def __init__(self):
         self.to_tensor = transforms.ToTensor()  # Convert PIL image to tensor
 
     def __call__(self, batch):
-        images, labels = zip(*batch)  # Unpack batch into separate lists
+        # Unpack batch into separate lists
+        images, labels = zip(*batch)
 
         # Convert images to tensors and get their shapes
         images = [self.to_tensor(img) for img in images]
@@ -15,20 +18,19 @@ class SynthCollator:
 
         # Create a tensor to hold the padded images
         max_width = max(widths)
-        padded_imgs = torch.zeros((len(images), channels, height, max_width), dtype=torch.float32)
-        
+        padded_imgs = torch.zeros((len(images), channels, height, max_width),
+                                  dtype=torch.float32)
+
         # Copy each image into the tensor with padding
         for i, img in enumerate(images):
             orig_width = widths[i]
-            
-            padded_imgs[i, :, :, : orig_width] = img
-            
+            padded_imgs[i, :, :, :orig_width] = img
             # add the same background for new padding
+            # if not, the added background will be black
             if orig_width < max_width:
                 padded_imgs[i, :, :, orig_width:] = img[:, :, -1].unsqueeze(2)
+
         return {
-            'images': padded_imgs,  # Batched image tensor
-            'labels': labels       # List of labels (strings)
+            "images": padded_imgs,  # Batched image tensor
+            "labels": labels,  # List of labels (strings)
         }
-
-
