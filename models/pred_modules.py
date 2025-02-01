6eea73b989f7f3402675280211c10391b7fe5037
mp1704 <pmphuong1704@gmail>
2025-02-01 13:14:05 +0700

    models

--- models/pred_modules.py
+++ models/pred_modules.py
@@ -1,17 +1,20 @@
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
-# CTC, Attention
-class CTC_Predictor(nn.Module):
-    def __init__(self, input_dim, num_classes):
+
+
+class CTC(nn.Module):
+    def __init__(self, input_dim , num_classes):
         super().__init__()
         self.fc = nn.Linear(input_dim, num_classes)
 
     def forward(self, x):
-        # torch.Size([16, 512, 2, 1])
-        # x shape: [batch_size, dim, height, width]
-        # b, c, h, w = x.size()
         return self.fc(x)
         
-       
+
 # Postprocessing: Greedy, Beam Search
+
+class Attention:
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        ...
+
+    def forward(self, x):
+        ...
