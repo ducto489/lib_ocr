3e1ddf436d02574ff2c03507dd260d76d9673c7b
mp1704 <pmphuong1704@gmail>
2025-02-01 13:21:45 +0700

    add collate

--- dataloader.py
+++ dataloader.py
@@ -3,7 +3,7 @@ from pytorch_lightning import LightningDataModule
 from torch.utils.data import DataLoader
 
 from data import OCRDataset
-
+from data.collate import OCRCollator
 
 class OCRDataModule(LightningDataModule):
     def __init__(self, train_data_path, val_data_path, batch_size=32, num_workers=4):
@@ -12,11 +12,12 @@ class OCRDataModule(LightningDataModule):
         self.val_data_path = val_data_path
         self.batch_size = batch_size
         self.num_workers = num_workers
+        self.collator = OCRCollator()
 
     def train_dataloader(self):
         self.train_data = OCRDataset(self.train_data_path)
-        return DataLoader(self.train_data, batch_size=self.batch_size, num_workers=self.num_workers)
+        return DataLoader(self.train_data, batch_size=self.batch_size, num_workers=self.num_workers, collate_fn=self.collator   )
 
     def val_dataloader(self):
         self.val_data = OCRDataset(self.val_data_path)
-        return DataLoader(self.val_data, batch_size=self.batch_size, num_workers=self.num_workers)
+        return DataLoader(self.val_data, batch_size=self.batch_size, num_workers=self.num_workers, collate_fn=self.collator)
