7075114c54e16639d400d027cd924790786bcc8a
mp1704 <pmphuong1704@gmail>
2025-02-04 06:29:18 +0700

    add fit to script

--- train.sh
+++ train.sh
@@ -1,9 +1,10 @@
 
-
-
-
-python cli.py train \
-    --backbone_name resnet18 \
-    --seq_name bilstm \
-    --pred_name ctc \
-    ...
+python cli.py fit \
+    --train_dir "training_images/images" \
+    --val_dir "validation_images/images" \
+    --backbone "resnet18" \
+    --seq_module "bilstm" \
+    --pred_module "ctc" \
+    --batch_size 64 \
+    --learning_rate 1e-3 \
+    --max_epochs 10
