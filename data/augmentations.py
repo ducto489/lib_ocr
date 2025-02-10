fbd1a5757560990667dc77525480f76399da6409
ducto489 <dustnn00@gmail.com>
2025-02-10 10:25:50 +0000

    Try fix the height but the width free

--- data/augmentations.py
+++ data/augmentations.py
@@ -4,9 +4,9 @@ import torchvision.transforms as transforms
 class Scaling:
     def __call__(self, image):
         w, h = image.size
-        W, H = (420, 100)
-        scale_ratio = min(W/w, H/h)
-        return transforms.functional.resize(image,(int(h*scale_ratio), int(w*scale_ratio)))
+        H = 100
+        scale_ratio = H/h
+        return transforms.functional.resize(image,(100, int(w*scale_ratio)))
 
 data_transforms = {
     "train": transforms.Compose(
@@ -14,7 +14,7 @@ data_transforms = {
             # transforms.Resize((100, 420)),
             Scaling(),
             # If the size is smaller than (100, 420), it will padding. Else it will crop. But above I have assertion if any image have size bigger than (100, 420)
-            transforms.CenterCrop((100, 420)),
+            # transforms.CenterCrop((100, 420)),
             # transforms.Grayscale(num_output_channels=1),
             transforms.ToTensor(),
             transforms.Normalize((0.5,), (0.5,)),
@@ -23,7 +23,7 @@ data_transforms = {
     "val": transforms.Compose(
         [
             Scaling(),
-            transforms.CenterCrop((100, 420)),
+            # transforms.CenterCrop((100, 420)),
             # transforms.Grayscale(num_output_channels=1),
             transforms.ToTensor(),
             transforms.Normalize((0.5,), (0.5,)),
--- train.sh
+++ train.sh
@@ -3,9 +3,9 @@
 python cli.py fit \
     --data.train_data_path "training_images" \
     --data.val_data_path "training_images" \
-    --data.batch_size 32 \
-    --model.backbone_name "vgg" \
-    --model.seq_name None \
+    --data.batch_size 64 \
+    --model.backbone_name "resnet18" \
+    --model.seq_name "bilstm" \
     --model.pred_name "ctc" \
     --model.learning_rate 1e-3 \
     --trainer.max_epochs 30 \
