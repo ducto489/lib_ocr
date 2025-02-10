5520aa1b2c2e8ec33321e73d93a9c182826f38fe
ducto489 <dustnn00@gmail.com>
2025-02-10 05:31:19 +0000

    fix vgg architecture for overfit

--- train.sh
+++ train.sh
@@ -10,5 +10,5 @@ python cli.py fit \
     --model.learning_rate 1e-3 \
     --trainer.max_epochs 30 \
     --trainer.logger WandbLogger \
-    --trainer.logger.name "VGG-real-1"\
+    --trainer.logger.name "VGG-real-test"\
     --trainer.logger.project "OCR"
