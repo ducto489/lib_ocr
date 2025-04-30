b0e0ab52b9602862a8f227732bc117d897cdb76e
ducto489 <dustnn00@gmail.com>
2025-04-30 17:30:26 +0700

    add dali aug

--- data/augmentations.py
+++ data/augmentations.py
@@ -31,9 +31,9 @@ data_transforms_2 = {
             Scaling(),
             v2.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.3),
             v2.RandomAffine(degrees=2, scale=(0.9, 1)),
-            v2.GaussianBlur((5, 9), sigma=(0.1, 2.)),
             v2.RandomPerspective(distortion_scale=0.1, p=0.5),
             v2.ToTensor(),
+            v2.GaussianNoise(),
             v2.Normalize((0.5,), (0.5,)),
         ]
     ),
