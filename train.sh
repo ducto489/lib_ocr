64bcb0ed308ab9f17736edca7ead4f03b568c5a3
ducto489 <dustnn00@gmail.com>
2025-05-01 00:46:08 +0900

    update requiremets

--- train.sh
+++ train.sh
@@ -5,16 +5,37 @@ python cli.py fit \
     --data.val_data_path "/home/qsvm/dataset/val" \
     --data.batch_size 64 \
     --data.num_workers 8 \
-    --data.dali False \
-    --data.frac 0.1 \
+    --data.dali True \
+    --data.frac 1 \
     --model.backbone_name "resnet18" \
     --model.seq_name "bilstm" \
     --model.pred_name "attn" \
     --model.learning_rate 1e-4 \
     --model.batch_max_length 50 \
-    --model.save_dir "checkpoints/run50" \
-    --trainer.max_epochs 1 \
+    --model.save_dir "checkpoints/runreal50" \
+    --trainer.max_epochs 10 \
+    --trainer.val_check_interval 0.5 \
     --trainer.logger WandbLogger \
-    --trainer.logger.name "train50-dali-aug"\
+    --trainer.logger.name "real-dali-50"\
+    --trainer.logger.project "OCR"\
+    --trainer.log_every_n_steps 16
+
+python cli.py fit \
+    --data.train_data_path "/home/qsvm/dataset/train" \
+    --data.val_data_path "/home/qsvm/dataset/val" \
+    --data.batch_size 64 \
+    --data.num_workers 8 \
+    --data.dali True \
+    --data.frac 1 \
+    --model.backbone_name "resnet18" \
+    --model.seq_name "bilstm" \
+    --model.pred_name "attn" \
+    --model.learning_rate 1e-4 \
+    --model.batch_max_length 200 \
+    --model.save_dir "checkpoints/runreal200" \
+    --trainer.max_epochs 10 \
+    --trainer.val_check_interval 0.5 \
+    --trainer.logger WandbLogger \
+    --trainer.logger.name "real-dali-200"\
     --trainer.logger.project "OCR"\
     --trainer.log_every_n_steps 16
